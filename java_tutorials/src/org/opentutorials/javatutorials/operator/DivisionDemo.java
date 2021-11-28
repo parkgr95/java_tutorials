3f423bb1fe15683acdc2f13d3dc8e3f3073e6e65
82106 <pkr951113@gmail.com>
2021-11-28 19:22:08 +0900

    1128

---
+++ java_tutorials/src/org/opentutorials/javatutorials/operator/DivisionDemo.java
@@ -0,0 +1,17 @@
+package org.opentutorials.javatutorials.operator;
+
+public class DivisionDemo {
+
+	public static void main(String[] args) {
+		int a = 10;
+	    int b = 3;
+
+	    float c = 10.0F;
+	    float d = 3.0F;
+
+	    System.out.println(a/b);  // 정수/정수
+	    System.out.println(c/d);  // 실수/실수
+	    System.out.println(a/d);  // 정수/실수
+	}
+
+}
